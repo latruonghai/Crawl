ada96531dfb37a34b8a5a1f96b88b76ba93fce57
= <=>
2020-06-26 12:16:12 +0700

    2

--- Crawl_Facebook.py
+++ Crawl_Facebook.py
@@ -1,14 +1,19 @@
-import bs4
 import pandas as pd
 import codecs
+import requests
+import bs4
+
 
 class Crawl:
-    
-    def __init__(self, url, is_sarcasm=[], Article=[], Headline=[]):
+
+    def __init__(self, url, mode):
         self.url = url
-        self.is_sarcasm = is_sarcasm
-        self.Article = Article
-        self.Headline = Headline
+        self.mode = mode
+        #self.is_sarcasm = is_sarcasm
+
+    def get_page_content(self):
+        page = requests.get(self.url)
+        return bs4.BeautifulSoup(page.text, "html.parser")
 
     def read_file(self):
         file = codecs.open(self.url, 'r')
@@ -16,37 +21,52 @@ class Crawl:
         file1 = str(file1)
         return bs4.BeautifulSoup(file1, "html.parser")
 
-    def parser(self):
+    def parser_link(self):
+        soup = self.get_page_content()
+        box = soup.findAll('span', class_="_2iem")
+        print(soup)
+
+    def get_mode(self):
+        self.mode = self.mode.lower()
+        switch_mode = {"post": "parser_post()", 'like': 'parser_file_like()'}
+        self.mode = switch_mode[self.mode]
+
+    def parser_post(self):
         soup = self.read_file()
+        links = soup.findAll('div', class_="qzhwtbm6 knvmm38d")
+        Links = [l.find('a', 
+                class_="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl oo9gr5id gpro0wi8 lrazzd5p") for l in links]
+        link = []
+        name = []
+        for l in Links:
+            if l != None:
+                li = l.get("href")
+                n = l.text
+                if li.find("hashtag") == -1:
+                    link.append(li)
+                    name.append(n)
+            get_true_string(link)
+        self.Link = link
+        self.Name = name
+
+    def parser_file_like(self):
+        soup = self.read_file()
+        print(soup)
         # Tìm các box chứa mục cần tìm
         #box = soup.findAll('div',class_="card__text")
         # Tìm link trong nó nếu có
-        links = soup.findAll('a',
-        class_="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl oo9gr5id gpro0wi8 lrazzd5p")
-
-        print(links)
+        links = soup.find('div',
+                          class_="a8s20v7p k5wvi7nf buofh1pr pfnyh3mw l9j0dhe7 du4w35lb")
+        links = links.findAll('a', class_="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl oo9gr5id gpro0wi8 lrazzd5p")
         if len(links) > 0:
-            link = [l.get('href') for l in links]
+            link = [l.find('a').get("href") for l in links]
+            get_true_string(link)
+            print(link)
         #headlines = [head.find('h2',class_="card__headline__text").text for head in box]
         headlines = [head.text for head in links]
-        self.Article = link
-        self.Headline = headlines
-        self.is_sarcasm = [1]*len(link)
-
-
-def get_df(Crawl):
-    papers = {"Article": Crawl.Article,
-              "Headline": Crawl.Headline, "is_sarcasm": Crawl.is_sarcasm}
-    data = pd.DataFrame(papers)
-    return data
-
-
-def get_Crawl(lists):
-    for url in lists:
-        crawl = Crawl(url)
-        crawl.parser()
-        data = get_df(crawl)
-        yield data
+        self.Link = link
+        self.Name = headlines
+        #self.is_sarcasm = [1]*len(link)
 
 
 def get_csv(listCrawl):
@@ -54,11 +74,36 @@ def get_csv(listCrawl):
     for i in range(len(listCrawl)):
         data1 = data1.append(listCrawl[i])
     filename = input("Nhap vao file name: ")
-    result = data1.to_csv(filename + ".csv", header=True, index=None)
+    result = data1.to_csv('Data/' + filename + ".csv", header=True, index=None)
     return result
 
 
+def get_true_string(strings):
+    for i in range(len(strings)):
+        if strings[i].find('__') != -1:
+            index = strings[i].find('__')
+            strings[i] = strings[i].replace(
+                strings[i], strings[i][0:index])
+
+
+def get_df(Crawl):
+    papers = {"Name": Crawl.Name,
+              "Link": Crawl.Link}
+    data = pd.DataFrame(papers)
+    return data
+
+
+def get_Crawl(lists):
+    mode = input("Nhap vao kieu can quet")
+    for url in lists:
+        crawl = Crawl(url, mode)
+        crawl.get_mode()
+        eval('crawl.' + crawl.mode)
+        data = get_df(crawl)
+        yield data
+
+
 if __name__ == "__main__":
-    lists = ["result.html"]
+    lists = ["GXVN1.html"]
     listCrawl = list(get_Crawl(lists))
     result = get_csv(listCrawl)
